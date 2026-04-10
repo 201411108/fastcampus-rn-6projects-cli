32f5bb113e562700a9636b3d2195669507f9195f
Handong Kim <aggsae@gmail.com>
2026-04-10 17:07:25 +0900

    chore: turbomodule 연습용 코드 제거

--- RnTurboPedometer/android/app/src/main/java/com/hankim/turbopedometer/MainApplication.kt
+++ RnTurboPedometer/android/app/src/main/java/com/hankim/turbopedometer/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.hankim.turbopedometer.nativestepsensor.NativeStepSonsorPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,7 +17,6 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(NativeStepSonsorPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
--- RnTurboPedometer/specs/NativeStepSensor.ts
+++
@@ -1,9 +0,0 @@
-import { TurboModule, TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-  isAvailable: () => Promise<boolean>;
-  getStepCount: () => Promise<number>;
-  getSensorName: () => Promise<string>;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('NativeStepSensor');
